12ba873e91b01c4db152968facc6790b4df631db
ayeniidris <61968571+ayeniidris@users.noreply.github.com>
2020-05-29 20:14:47 -0700

    Added Authentication db context class

--- src/EstateApp.Data/DatabaseContexts/AuthenticationDbcontext/AuthenticationDbcontext.cs
+++ src/EstateApp.Data/DatabaseContexts/AuthenticationDbcontext/AuthenticationDbcontext.cs
@@ -1,7 +1,14 @@
+using EstateApp.Data.Entities;
+using Microsoft.AspNetcore.Identity.EntityFrameworkcore;
+
 namespace EstateApp.Data.Databasecontexts.AuthenticationDbcontext
 {
-    public class AuthenticationDbcontext
+    public class AuthenticationDbcontext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbcontexT(DbContextoptions<AuthenticationDbContext> options)
+            :base(options)
+        {
+            
+        }
     }
 }
---
+++ src/EstateApp.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetcore.Identity;
+
+namespace EstateApp.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
